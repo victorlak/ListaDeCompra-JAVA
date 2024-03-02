ad36987cc6384fef5a48c3b4ee80b22e5f54cee6
victor <133174073+victorlak@users.noreply.github.com>
2024-03-02 12:17:32 -0300

    Add files via upload

---
+++ ProjetoPedro.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetopedro;
+
+/**
+ *
+ * @author Victor
+ */
+public class ProjetoPedro {
+
+    public static void main(String[] args) {
+        ListaDeCompras lista = new ListaDeCompras(5);
+        Produto p = new Produto();
+        lista.adicionarProduto();
+        lista.adicionarProduto();
+        lista.adicionarProduto();
+        lista.adicionarProduto();
+        lista.adicionarProduto();
+        lista.removerProduto();
+        lista.listarProdutos();
+        
+               
+        
+        
+     
+    }
+}
